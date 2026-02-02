3bc1d5288abc6b11e64a51529e9cafb19768dfac
CalderWStudent <cworthington20@student.cccs.edu>
2026-02-02 11:29:13 -0700

    Changed waitTime values

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Restaurant burgerKing = new Restaurant(25, 4.3, 15, 10, "9 am - 11 pm");
-        Restaurant wendys = new Restaurant(30, 4.5, 14, 15, "8 am - 10 pm");
+        Restaurant burgerKing = new Restaurant(4, 4.3, 15, 10, "9 am - 11 pm");
+        Restaurant wendys = new Restaurant(5, 4.5, 14, 15, "8 am - 10 pm");
 
         // Prints the wait time for both restaurants.
-        System.out.println("Burger King Wait Time: " + burgerKing.waitTime);
-        System.out.println("Wendy's Wait Time: " + wendys.waitTime);
+        System.out.println("Burger King Wait Time: " + burgerKing.waitTime + "m");
+        System.out.println("Wendy's Wait Time: " + wendys.waitTime + "m");
     }
 }
